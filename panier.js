7b16440ae33b063bb80c017a9c2b06b95d715f6e
Haltay <joachim.recio@gmail.com>
2021-04-01 11:08:38 +0100

    Done Formulaire Todo envoie vers le serveur
    Le formulaire est fait, maintenant il faut l'envoyer au serveur (enfin pas le formulaire, que ses données)

--- panier.js
+++ panier.js
@@ -106,10 +106,182 @@ panierItem.insertAdjacentHTML("afterend", affichePrixPanier);
 
 // ----------------------------- Formulaire de commande --------------------------
 
+const affiherFormulaireHtml = () => {
+
+    const FormulaireCommande = document.querySelector(".formulaire");
+
+    const structureFormulaire = `
+    <div class="col-md-8 order-md-1">
+        <h4 class="mb-3">Adresse de facturation</h4>
+        <form class="needs-validation" novalidate>
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label for="firstName">Prénom</label>
+                    <input type="text" class="form-control" id="prenom" placeholder="" value="" required>
+                    <div class="invalid-feedback">
+                        Merci d'indiquer ton prénom
+                    </div>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label for="lastName">Nom de famille</label>
+                    <input type="text" class="form-control" id="nom" placeholder="" value="" required>
+                    <div class="invalid-feedback">
+                    Merci d'indiquer ton nom de famille
+                    </div>
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" placeholder="you@example.com">
+                <div class="invalid-feedback">
+                Merci d'entrer une adresse email valide
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="address">Adresse</label>
+                <input type="text" class="form-control" id="adresse" placeholder="1234 Main St" required>
+                <div class="invalid-feedback">
+                Merci d'indiquer ton adresse de livraison
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="address2">Complément d'adresse <span class="text-muted">(Optionel)</span></label>
+                <input type="text" class="form-control" id="adresse2" placeholder="Apartment or suite">
+            </div>
+
+            <div class="row">
+                <div class="col-md-5 mb-3">
+                <label for="country">Pays</label>
+                <select class="custom-select d-block w-100" id="pays" required>
+                    <option value="">C'est ...</option>
+                    <option>France</option>
+                    <option>Royaume-Uni</option>
+                    <option>Belgique</option>
+                    <option>Wakanda</option>
+                </select>
+                <div class="invalid-feedback">
+                    Merci de sélectionner ton pays
+                </div>
+                </div>
+            
+                <div class="col-md-3 mb-3">
+                <label for="zip">Code postal</label>
+                <input type="text" class="form-control" id="code-postal" placeholder="" required>
+                <div class="invalid-feedback">
+                    J'ai besoin de ton code postal
+                </div>
+                </div>
+            </div>
+            <hr class="mb-4">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="same-address">
+                <label class="custom-control-label" for="same-address">L'adresse de livraison est la même que l'adresse de facturation</label>
+            </div>
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="save-info">
+                <label class="custom-control-label" for="save-info">Sauvegarder mes infos pour la prochaine fois</label>
+            </div>
+            <hr class="mb-4">
+
+            <h4 class="mb-3">Paiement</h4>
+
+            <div class="d-block my-3">
+                <div class="custom-control custom-radio">
+                <input id="credit" name="paymentMethod" type="radio" class="custom-control-input" checked required>
+                <label class="custom-control-label" for="credit">Credit card</label>
+                </div>
+                <div class="custom-control custom-radio">
+                <input id="debit" name="paymentMethod" type="radio" class="custom-control-input" required>
+                <label class="custom-control-label" for="debit">Debit card</label>
+                </div>
+                <div class="custom-control custom-radio">
+                <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required>
+                <label class="custom-control-label" for="paypal">PayPal</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                <label for="cc-name">Nom sur la carte</label>
+                <input type="text" class="form-control" id="cc-name" placeholder="" required>
+                <small class="text-muted">Recopie le nom entier sur ta carte</small>
+                <div class="invalid-feedback">
+                    Name on card is required
+                </div>
+                </div>
+                <div class="col-md-6 mb-3">
+                <label for="cc-number">Numéro de la carte</label>
+                <input type="text" class="form-control" id="cc-number" placeholder="" required>
+                <div class="invalid-feedback">
+                    Pas de numérao de carte, pas de Teddy
+                </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 mb-3">
+                <label for="cc-expiration">Expiration</label>
+                <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
+                <div class="invalid-feedback">
+                    La date d'expiration est demandée
+                </div>
+                </div>
+                <div class="col-md-3 mb-3">
+                <label for="cc-cvv">CVV</label>
+                <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
+                <div class="invalid-feedback">
+                    Plus que le CVV est c'est fini
+                </div>
+                </div>
+            </div>
+            <hr class="mb-4">
+            <button class="btn btn-primary btn-lg btn-block" id="envoyer-formulaire" type="submit">Continue to checkout</button>
+            </form>
+        </div>
+    </div>
+    `;
+
+    FormulaireCommande.insertAdjacentHTML("afterend", structureFormulaire);
+};
+
+affiherFormulaireHtml ();
+
+
+// Récuperation des données du formulaire (hors carte crédit) pour local storage
+
+const btnCheckout = document.querySelector("#envoyer-formulaire");
+
+btnCheckout.addEventListener("click", (e) => {
+e.preventDefault();
+
+localStorage.setItem("prenom", document.querySelector("#prenom").value);
+localStorage.setItem("nom", document.querySelector("#nom").value);
+localStorage.setItem("email", document.querySelector("#email").value);
+localStorage.setItem("adresse", document.querySelector("#adresse").value);
+localStorage.setItem("adresse2", document.querySelector("#adresse2").value);
+localStorage.setItem("pays", document.querySelector("#pays").value);
+localStorage.setItem("codePostal", document.querySelector("#code-postal").value);
+
+const formulaire = {
+    prenom: localStorage.getItem("prenom"),
+    nom: localStorage.getItem("nom"),
+    email: localStorage.getItem("email"),
+    adresse: localStorage.getItem("adresse"),
+    adresse2: localStorage.getItem("adresse2"),
+    pays: localStorage.getItem("pays"),
+    codePostal: localStorage.getItem("codePostal"),
+}
+
+// Pour envoyer au serveur
+const aEnvoyerServeur = {
+    cartItem,
+    formulaire,
+}
+
+// Envoie de "aEnvoyerServeur" vers le serveur
 
 
 
-
-
-
+})
 
