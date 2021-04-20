b62392249938f3d8ab0d4a8f306d0eae7db24877
Haltay <joachim.recio@gmail.com>
2021-04-20 10:19:39 +0100

    Done, Mise en place des noms des teddys dans la "confirmation", et uuid

--- confirmation.js
+++ confirmation.js
@@ -1,11 +1,13 @@
 let cartItem = JSON.parse(localStorage.getItem("product"));
 let formulaireValues = JSON.parse(localStorage.getItem("formulaireValues"));
+console.log(cartItem);
+console.log(formulaireValues);
 
 const confirmation = document.querySelector(".confirmation");
 
-   // ToDO voir pourquoi un seul nom de Teddy et pas tous.
-cartItem.forEach((item) => {
-    confirmation.innerHTML = `
+// Affichage de la card Confirmation et des noms du/des Teddy
+
+const confirmationDetails = `
         <div class="col-md-5 mt-5 confirmation-achat">
             <h3 class="text pt-4 nom-prenom">${formulaireValues.prenom} ${formulaireValues.nom}</h3>
             <h4 class="mt-5 theme-color mb-5">Merci pour votre achat n°${formulaireValues.numeroCommande}</h4> 
@@ -13,7 +15,7 @@ cartItem.forEach((item) => {
             <div class="mb-3">
                 <hr class="new1">
             </div>
-            <div class="d-flex justify-content-between"> <span class="font-weight-bold">Bientôt arrivera chez vous: ${item.nom}</span> <span class="text-muted">0 €</span> </div>
+            <div class="d-flex justify-content-between"> <span class="font-weight-bold">Bientôt arrivera chez vous: <span class="nameTeddy"> </span> </span> <span class="text-muted">0 €</span> </div>
             <div class="d-flex justify-content-between"> <small>Frais de dossier</small> <small>${formulaireValues.panierTotal} €</small> </div>
             <div class="d-flex justify-content-between"> <small>Taxes</small> <small>Aucune l'amitié n'a pas de prix</small> </div>
             <div class="d-flex justify-content-between mt-3"> <span class="font-weight-bold">Total à payer</span> <span class="font-weight-bold theme-color">${formulaireValues.panierTotal} €</span> </div>
@@ -27,8 +29,15 @@ cartItem.forEach((item) => {
             <div class="text-center mt-5 pb-4"> <button class="btn btn-primary" id="retourAccueil">Revoir tous les Teddys</button> </div>
         </div>`;
 
- console.log(item.nom);
-})
+confirmation.innerHTML = confirmationDetails;
+
+    // Affichage du nom du/des Teddy commandé(s)
+const displayName = document.querySelector(".nameTeddy");
+cartItem.forEach((item) => {
+    displayName.innerHTML +=  `
+        ${item.nom} 
+    `;    
+});
 
 
 // Retour à l'accueil et suppression du panier
